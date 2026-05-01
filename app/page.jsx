55c9d3ba91d8e27e050062ae5d057d59a68a1193
Makagyn Grimm <makagyn11@gmail.com>
2026-05-01 01:42:24 -0500

    Handle Root error

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,12 @@ import css from './styles/main.css';
 import {getCredentials,getHostTotals,getLicenseTotals,getHostList} from "./actions/ncompass";
 
 export default async function Root() {
-  const {nctvAuthToken} = await getCredentials();
-  const data = await getLicenseTotals({nctvAuthToken});
-  console.log(data)
-  return <div>Welcome to Root</div>;
+  try {
+    const {nctvAuthToken} = await getCredentials();
+    const data = await getLicenseTotals({nctvAuthToken});
+    console.log(data)
+    return <div>Welcome to Root</div>;
+  } catch (err) {
+    return <div>Something went wrong</div>;
+  }
 }
