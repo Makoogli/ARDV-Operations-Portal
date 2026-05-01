413df596e3ccd8a0aa3ced630835cc0b962929d9
Makagyn Grimm <makagyn11@gmail.com>
2026-05-01 01:37:50 -0500

    Handle Dashboard fetch error

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,8 @@
 export default async function Dashboard() {
-  console.log("url",`${process.env.SITE_URL}/api/ncompass/credentials`);
   const response = await fetch(`${process.env.SITE_URL}/api/ncompass/credentials`);
+  if(response.status != 200){
+    return <div>Something went wrong</div>
+  }
   const credentials = await response.json();
-  console.log("credentials",credentials);
-  console.log(credentials);
   return <div>Welcome to Operations Dashboard</div>;
 }
